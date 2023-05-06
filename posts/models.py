3ff6239dff0fb3176eb64b1657b3e7666d189117
MahmoudAhmed <mahmoudahmed@MahmoudAhmeds-MacBook-Pro.local>
2023-05-06 22:10:14 +0300

    add created_at , url in template

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from taggit.managers import TaggableManager
+from django.utils import timezone
 
 # Create your models here.
 class Author(models.Model):
@@ -17,6 +18,7 @@ class Post(models.Model):
     image = models.ImageField(upload_to='posts/')
     author = models.ForeignKey(Author , on_delete=models.CASCADE , related_name='post_author')
     tags = TaggableManager()
+    created_at = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
         return self.title
