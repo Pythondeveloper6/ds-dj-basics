efe1857e0c67b3fdb8685632462be39ec4af6189
MahmoudAhmed <mahmoudahmed@MahmoudAhmeds-MacBook-Pro.local>
2023-05-09 22:32:29 +0300

    add crud in api - update issue

--- posts/api.py
+++ posts/api.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView , RetrieveAPIView
+from rest_framework.generics import ListAPIView , RetrieveAPIView ,UpdateAPIView,CreateAPIView,DestroyAPIView
 from .models import Post
 from .serializers import PostSerializer
 
@@ -11,3 +11,19 @@ class PostAPI(ListAPIView):
 class PostDetailAPI(RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    
+    
+class PostUpdateAPI(UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+    
+class PostCreateAPI(CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+    
+class PostDeleteAPi(DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    # success_url = ''
